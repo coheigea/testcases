64e356742e9f09cc698356929eeb229e821175d9
Colm O hEigeartaigh <coheigea@apache.org>
2014-03-07 11:34:34 +0000

    Added some initial testcases for using Apache Shiro with CXF for authentication

---
+++ apache/cxf/cxf-shiro/src/test/java/org/apache/coheigea/cxf/shiro/authentication/ShiroBasicAuthInterceptor.java
@@ -0,0 +1,119 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.coheigea.cxf.shiro.authentication;
+
+import java.security.Principal;
+import java.util.logging.Logger;
+
+import org.w3c.dom.Document;
+import org.apache.cxf.common.logging.LogUtils;
+import org.apache.cxf.configuration.security.AuthorizationPolicy;
+import org.apache.cxf.helpers.DOMUtils;
+import org.apache.cxf.interceptor.Fault;
+import org.apache.cxf.message.Message;
+import org.apache.cxf.phase.AbstractPhaseInterceptor;
+import org.apache.cxf.phase.Phase;
+import org.apache.cxf.security.SecurityContext;
+import org.apache.ws.security.WSConstants;
+import org.apache.ws.security.WSUsernameTokenPrincipal;
+import org.apache.ws.security.handler.RequestData;
+import org.apache.ws.security.message.token.UsernameToken;
+import org.apache.ws.security.validate.Credential;
+import org.apache.ws.security.validate.Validator;
+
+public class ShiroBasicAuthInterceptor extends AbstractPhaseInterceptor<Message> {
+
+    private static final Logger LOG = LogUtils.getL7dLogger(ShiroBasicAuthInterceptor.class);
+    
+    private Validator validator;
+    
+    public ShiroBasicAuthInterceptor() {
+        this(Phase.UNMARSHAL);
+    }
+    
+    public ShiroBasicAuthInterceptor(String phase) {
+        super(phase);
+    }
+    
+    public void handleMessage(Message message) throws Fault {
+
+        AuthorizationPolicy policy = message.get(AuthorizationPolicy.class);
+        if (policy == null || policy.getUserName() == null || policy.getPassword() == null) {
+            String name = null;
+            if (policy != null) {
+                name = policy.getUserName();
+            }
+            String error = "No user credentials are available";
+            LOG.warning(error + " " + "for name: " + name);
+            throw new SecurityException(error);
+        }
+        
+        try {
+            UsernameToken token = convertPolicyToToken(policy);
+            Credential credential = new Credential();
+            credential.setUsernametoken(token);
+            
+            RequestData data = new RequestData();
+            data.setMsgContext(message);
+            credential = validator.validate(credential, data);
+            
+            // Create a Principal/SecurityContext
+            Principal p = null;
+            if (credential != null && credential.getPrincipal() != null) {
+                p = credential.getPrincipal();
+            } else {
+                p = new WSUsernameTokenPrincipal(policy.getUserName(), false);
+                ((WSUsernameTokenPrincipal)p).setPassword(policy.getPassword());
+            }
+            message.put(SecurityContext.class, createSecurityContext(p));
+            
+        } catch (Exception ex) {
+            throw new Fault(ex);
+        }
+    }
+
+    protected UsernameToken convertPolicyToToken(AuthorizationPolicy policy) 
+        throws Exception {
+
+        Document doc = DOMUtils.createDocument();
+        UsernameToken token = new UsernameToken(false, doc, 
+                                                WSConstants.PASSWORD_TEXT);
+        token.setName(policy.getUserName());
+        token.setPassword(policy.getPassword());
+        return token;
+    }
+    
+    protected SecurityContext createSecurityContext(final Principal p) {
+        return new SecurityContext() {
+
+            public Principal getUserPrincipal() {
+                return p;
+            }
+
+            public boolean isUserInRole(String arg0) {
+                return false;
+            }
+        };
+    }
+
+    public void setValidator(Validator validator) {
+        this.validator = validator;
+    }
+    
+}
---
+++ apache/cxf/cxf-shiro/src/test/java/org/apache/coheigea/cxf/shiro/authentication/ShiroUTValidator.java
@@ -0,0 +1,91 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.coheigea.cxf.shiro.authentication;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
+import org.apache.ws.security.WSConstants;
+import org.apache.ws.security.WSSecurityException;
+import org.apache.ws.security.handler.RequestData;
+import org.apache.ws.security.message.token.UsernameToken;
+import org.apache.ws.security.validate.Credential;
+import org.apache.ws.security.validate.Validator;
+
+/**
+ * This is a custom Validator that authenticates via Apache Shiro.
+ */
+public class ShiroUTValidator implements Validator {
+    
+    private static org.apache.commons.logging.Log log = 
+            org.apache.commons.logging.LogFactory.getLog(ShiroUTValidator.class);
+    
+    public ShiroUTValidator(String iniResourcePath) {
+        Factory<SecurityManager> factory = new IniSecurityManagerFactory(iniResourcePath);
+        SecurityUtils.setSecurityManager(factory.getInstance());
+    }
+    
+    public Credential validate(Credential credential, RequestData data) throws WSSecurityException {
+        if (credential == null || credential.getUsernametoken() == null) {
+            throw new WSSecurityException(WSSecurityException.FAILURE, "noCredential");
+        }
+        
+        // Validate the UsernameToken
+        UsernameToken usernameToken = credential.getUsernametoken();
+        String pwType = usernameToken.getPasswordType();
+        if (log.isDebugEnabled()) {
+            log.debug("UsernameToken user " + usernameToken.getName());
+            log.debug("UsernameToken password type " + pwType);
+        }
+        if (!WSConstants.PASSWORD_TEXT.equals(pwType)) {
+            if (log.isDebugEnabled()) {
+                log.debug("Authentication failed - digest passwords are not accepted");
+            }
+            throw new WSSecurityException(WSSecurityException.FAILED_AUTHENTICATION);
+        }
+        if (usernameToken.getPassword() == null) {
+            if (log.isDebugEnabled()) {
+                log.debug("Authentication failed - no password was provided");
+            }
+            throw new WSSecurityException(WSSecurityException.FAILED_AUTHENTICATION);
+        }
+        
+        // Validate it via Shiro
+        Subject currentUser = SecurityUtils.getSubject();
+        UsernamePasswordToken token = 
+            new UsernamePasswordToken(usernameToken.getName(), usernameToken.getPassword());
+        try {
+            currentUser.login(token);
+        } catch (AuthenticationException ex) {
+            if (log.isDebugEnabled()) {
+                log.debug(ex.getMessage(), ex);
+            }
+            throw new WSSecurityException(WSSecurityException.FAILED_AUTHENTICATION);
+        }
+        
+        return credential;
+    }
+    
+    
+}
