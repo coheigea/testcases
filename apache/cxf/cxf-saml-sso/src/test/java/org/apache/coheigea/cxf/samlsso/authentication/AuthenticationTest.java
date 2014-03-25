8f850a9485769a8e2e6ed8483e56c048669050ab
Colm O hEigeartaigh <coheigea@apache.org>
2014-03-25 17:04:23 +0000

    Added a new cxf-saml-sso project

---
+++ apache/cxf/cxf-saml-sso/src/test/java/org/apache/coheigea/cxf/samlsso/authentication/AuthenticationTest.java
@@ -0,0 +1,107 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.coheigea.cxf.samlsso.authentication;
+
+import java.net.URL;
+
+import javax.ws.rs.RedirectionException;
+
+import org.apache.coheigea.cxf.samlsso.idp.IdpServer;
+import org.apache.cxf.jaxrs.client.WebClient;
+import org.apache.cxf.testutil.common.AbstractBusClientServerTestBase;
+import org.junit.BeforeClass;
+
+/**
+ */
+public class AuthenticationTest extends AbstractBusClientServerTestBase {
+    
+    private static final String PORT = allocatePort(Server.class);
+    static final String IDP_PORT = allocatePort(IdpServer.class);
+    
+    @BeforeClass
+    public static void startServers() throws Exception {
+        assertTrue(
+                   "Server failed to launch",
+                   // run the server in the same process
+                   // set this to false to fork
+                   launchServer(Server.class, true)
+        );
+        assertTrue(
+                "Server failed to launch",
+                // run the server in the same process
+                // set this to false to fork
+                launchServer(IdpServer.class, true)
+        );
+    }
+   
+    @org.junit.Test
+    public void testAuthenticatedRequest() throws Exception {
+
+        // https://localhost:9001/doubleit/services/doubleit-rs/25
+        System.out.println("Sleeping...");
+        Thread.sleep(60 * 1000);
+        /*
+        URL busFile = AuthenticationTest.class.getResource("cxf-client.xml");
+
+        String address = "https://localhost:" + PORT + "/doubleit/services/doubleit-rs";
+        WebClient client = WebClient.create(address, "alice", "security", busFile.toString());
+        // WebClient.getConfig(client).getHttpConduit().getClient().setAutoRedirect(true);
+        client.type("text/plain").accept("text/plain");
+        
+        try {
+            client.path("/25");
+            client.get();
+        } catch (RedirectionException ex) {
+            //
+        }
+        
+        String location = (String)client.getResponse().getMetadata().getFirst("Location");
+        WebClient idpClient = WebClient.create(location, "alice", "security", busFile.toString());
+        idpClient.type("text/plain").accept("text/plain");
+        WebClient.getConfig(idpClient).getHttpConduit().getClient().setAutoRedirect(true);
+        WebClient.getConfig(idpClient).getRequestContext().put(
+                org.apache.cxf.message.Message.MAINTAIN_SESSION, Boolean.TRUE);
+        WebClient.getConfig(idpClient).getResponseContext().put(
+                org.apache.cxf.message.Message.MAINTAIN_SESSION, Boolean.TRUE);
+
+
+        for (String header : client.getResponse().getHeaders().keySet()) {
+            if (client.getResponse().getHeaders().get(header) != null 
+                    && !client.getResponse().getHeaders().get(header).isEmpty()) {
+                idpClient.getHeaders().add(header, (String)client.getResponse().getHeaders().get(header).get(0));
+            }
+        }
+        idpClient.get();
+        
+        /*
+        int numToDouble = 25;
+        int resp = client.post(numToDouble, Integer.class);
+        assertEquals(numToDouble * 2 , resp);
+        // assertEquals(500, ex.getResponse().getStatus());
+         */
+    }
+    
+    @org.junit.Test
+    @org.junit.Ignore
+    public void testUnauthenticatedRequest() throws Exception {
+        // TODO
+    }
+    
+    
+}
---
+++ apache/cxf/cxf-saml-sso/src/test/java/org/apache/coheigea/cxf/samlsso/idp/BasicAuthFilter.java
@@ -0,0 +1,113 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.coheigea.cxf.samlsso.idp;
+
+import java.security.Principal;
+
+import javax.security.auth.callback.CallbackHandler;
+import javax.ws.rs.core.Response;
+
+import org.apache.cxf.configuration.security.AuthorizationPolicy;
+import org.apache.cxf.helpers.DOMUtils;
+import org.apache.cxf.interceptor.Fault;
+import org.apache.cxf.jaxrs.ext.RequestHandler;
+import org.apache.cxf.jaxrs.model.ClassResourceInfo;
+import org.apache.cxf.message.Message;
+import org.apache.cxf.security.SecurityContext;
+import org.apache.ws.security.WSConstants;
+import org.apache.ws.security.WSUsernameTokenPrincipal;
+import org.apache.ws.security.handler.RequestData;
+import org.apache.ws.security.message.token.UsernameToken;
+import org.apache.ws.security.validate.Credential;
+import org.apache.ws.security.validate.UsernameTokenValidator;
+import org.w3c.dom.Document;
+
+/**
+ * A simple filter to validate a Basic Auth username/password via a CallbackHandler
+ */
+public class BasicAuthFilter implements RequestHandler {
+
+    private CallbackHandler callbackHandler;
+    
+    @Override
+    public Response handleRequest(Message message, ClassResourceInfo arg1) {
+        AuthorizationPolicy policy = message.get(AuthorizationPolicy.class);
+        if (policy == null || policy.getUserName() == null || policy.getPassword() == null) {
+            return Response.status(401).header("WWW-Authenticate", "Basic realm=\"Reservations\"").build();
+        }
+
+        try {
+            UsernameToken token = convertPolicyToToken(policy);
+            Credential credential = new Credential();
+            credential.setUsernametoken(token);
+            
+            RequestData data = new RequestData();
+            data.setMsgContext(message);
+            data.setCallbackHandler(callbackHandler);
+            UsernameTokenValidator validator = new UsernameTokenValidator();
+            credential = validator.validate(credential, data);
+            
+            // Create a Principal/SecurityContext
+            Principal p = null;
+            if (credential != null && credential.getPrincipal() != null) {
+                p = credential.getPrincipal();
+            } else {
+                p = new WSUsernameTokenPrincipal(policy.getUserName(), false);
+                ((WSUsernameTokenPrincipal)p).setPassword(policy.getPassword());
+            }
+            message.put(SecurityContext.class, createSecurityContext(p));
+            return null;
+        } catch (Exception ex) {
+            throw new Fault(ex);
+        }
+    }
+
+    protected UsernameToken convertPolicyToToken(AuthorizationPolicy policy) 
+        throws Exception {
+
+        Document doc = DOMUtils.createDocument();
+        UsernameToken token = new UsernameToken(false, doc, 
+                                                WSConstants.PASSWORD_TEXT);
+        token.setName(policy.getUserName());
+        token.setPassword(policy.getPassword());
+        return token;
+    }
+    
+    protected SecurityContext createSecurityContext(final Principal p) {
+        return new SecurityContext() {
+
+            public Principal getUserPrincipal() {
+                return p;
+            }
+
+            public boolean isUserInRole(String arg0) {
+                return false;
+            }
+        };
+    }
+
+    public CallbackHandler getCallbackHandler() {
+        return callbackHandler;
+    }
+
+    public void setCallbackHandler(CallbackHandler callbackHandler) {
+        this.callbackHandler = callbackHandler;
+    }
+
+}
