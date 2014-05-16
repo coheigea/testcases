f72f038e76c2e7e7a5cb92b16b9998d85e4b0580
Colm O hEigeartaigh <coheigea@apache.org>
2014-05-16 15:53:57 +0100

    Adding a cxf-kerberos testcase

---
+++ apache/cxf/cxf-kerberos/src/test/java/org/apache/coheigea/cxf/kerberos/authentication/AuthenticationTest.java
@@ -0,0 +1,107 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.coheigea.cxf.kerberos.authentication;
+
+import java.io.File;
+import java.net.URL;
+
+import javax.xml.namespace.QName;
+import javax.xml.ws.Service;
+
+import org.apache.cxf.Bus;
+import org.apache.cxf.bus.spring.SpringBusFactory;
+import org.apache.cxf.testutil.common.AbstractBusClientServerTestBase;
+import org.apache.wss4j.dom.WSSConfig;
+import org.apache.wss4j.integration.test.common.KerberosServiceStarter;
+import org.example.contract.doubleit.DoubleItPortType;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+
+/**
+ * A test-case that shows how to authenticate a JAX-WS client via Kerberos. A KDC is started,
+ * and the client obtains a service ticket, passing it in the security header of the service
+ * request, where it is validated by the service.
+ */
+public class AuthenticationTest extends AbstractBusClientServerTestBase {
+    
+    private static final String NAMESPACE = "http://www.example.org/contract/DoubleIt";
+    private static final QName SERVICE_QNAME = new QName(NAMESPACE, "DoubleItService");
+    
+    private static final String PORT = allocatePort(Server.class);
+    
+    private static boolean kerberosServerStarted = false;
+    
+    @BeforeClass
+    public static void startServers() throws Exception {
+        assertTrue(
+                   "Server failed to launch",
+                   // run the server in the same process
+                   // set this to false to fork
+                   launchServer(Server.class, true)
+        );
+        
+        WSSConfig.init();
+        
+        kerberosServerStarted = KerberosServiceStarter.startKerberosServer();
+
+        String basedir = System.getProperty("basedir");
+        if (basedir == null) {
+            basedir = new File(".").getCanonicalPath();
+        }
+
+        System.setProperty("sun.security.krb5.debug", "true");
+        System.setProperty("java.security.auth.login.config", 
+                           basedir + "/src/test/resources/kerberos/kerberos.jaas");
+        System.setProperty("java.security.krb5.conf",
+                           basedir + "/src/test/resources/kerberos/krb5.conf");
+    }
+    
+    @AfterClass
+    public static void tearDown() throws Exception {
+        if (kerberosServerStarted) {
+            KerberosServiceStarter.stopKerberosServer();
+        }
+    }
+
+    @org.junit.Test
+    public void testAuthenticatedRequest() throws Exception {
+
+        SpringBusFactory bf = new SpringBusFactory();
+        URL busFile = AuthenticationTest.class.getResource("cxf-client.xml");
+
+        Bus bus = bf.createBus(busFile.toString());
+        SpringBusFactory.setDefaultBus(bus);
+        SpringBusFactory.setThreadDefaultBus(bus);
+        
+        URL wsdl = AuthenticationTest.class.getResource("DoubleIt.wsdl");
+        Service service = Service.create(wsdl, SERVICE_QNAME);
+        QName portQName = new QName(NAMESPACE, "DoubleItTransportPort");
+        DoubleItPortType transportPort = 
+            service.getPort(portQName, DoubleItPortType.class);
+        updateAddressPort(transportPort, PORT);
+        
+        doubleIt(transportPort, 25);
+    }
+    
+    private static void doubleIt(DoubleItPortType port, int numToDouble) {
+        int resp = port.doubleIt(numToDouble);
+        assertEquals(numToDouble * 2 , resp);
+    }
+    
+}
