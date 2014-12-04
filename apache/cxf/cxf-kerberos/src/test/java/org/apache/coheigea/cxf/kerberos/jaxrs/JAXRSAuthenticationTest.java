7771ba3527277615cffb17f84a68105393209f0a
Colm O hEigeartaigh <coheigea@apache.org>
2014-12-04 11:42:40 +0000

    Updating to use annotation based approach of configuring the Apache DS KDC

--- apache/cxf/cxf-kerberos/src/test/java/org/apache/coheigea/cxf/kerberos/jaxrs/JAXRSAuthenticationTest.java
+++ apache/cxf/cxf-kerberos/src/test/java/org/apache/coheigea/cxf/kerberos/jaxrs/JAXRSAuthenticationTest.java
@@ -19,52 +19,135 @@
 package org.apache.coheigea.cxf.kerberos.jaxrs;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.net.URL;
 import java.util.Map;
 
 import javax.ws.rs.core.Response;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.cxf.jaxrs.client.WebClient;
 import org.apache.cxf.testutil.common.AbstractBusClientServerTestBase;
+import org.apache.cxf.testutil.common.TestUtil;
 import org.apache.cxf.transport.http.auth.SpnegoAuthSupplier;
-import org.apache.wss4j.integration.test.common.KerberosServiceStarter;
+import org.apache.directory.server.annotations.CreateKdcServer;
+import org.apache.directory.server.annotations.CreateLdapServer;
+import org.apache.directory.server.annotations.CreateTransport;
+import org.apache.directory.server.core.annotations.ApplyLdifFiles;
+import org.apache.directory.server.core.annotations.CreateDS;
+import org.apache.directory.server.core.annotations.CreateIndex;
+import org.apache.directory.server.core.annotations.CreatePartition;
+import org.apache.directory.server.core.integ.AbstractLdapTestUnit;
+import org.apache.directory.server.core.integ.FrameworkRunner;
+import org.apache.directory.server.core.kerberos.KeyDerivationInterceptor;
+import org.apache.wss4j.dom.WSSConfig;
 import org.ietf.jgss.GSSName;
-import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
 
 /**
  * This is a test-case that shows how to use Kerberos with a JAX-RS service.
  */
-public class JAXRSAuthenticationTest extends AbstractBusClientServerTestBase {
+
+@RunWith(FrameworkRunner.class)
+
+//Define the DirectoryService
+@CreateDS(name = "KerberosTest-class",
+  enableAccessControl = false,
+  allowAnonAccess = false,
+  enableChangeLog = true,
+  partitions = {
+      @CreatePartition(
+          name = "example",
+          suffix = "dc=example,dc=com",
+          indexes = {
+              @CreateIndex(attribute = "objectClass"),
+              @CreateIndex(attribute = "dc"),
+              @CreateIndex(attribute = "ou")
+          } )
+      },
+  additionalInterceptors = {
+     KeyDerivationInterceptor.class
+  }
+)
+
+@CreateLdapServer(
+  transports = {
+      @CreateTransport(protocol = "LDAP")
+  }
+)
+
+@CreateKdcServer(
+  transports = {
+      // @CreateTransport(protocol = "TCP", address = "127.0.0.1", port=1024)
+      @CreateTransport(protocol = "UDP", address = "127.0.0.1")
+  },
+  primaryRealm = "service.ws.apache.org",
+  kdcPrincipal = "krbtgt/service.ws.apache.org@service.ws.apache.org"
+)
+
+//Inject an file containing entries
+@ApplyLdifFiles("kerberos/kerberos.ldif")
+
+public class JAXRSAuthenticationTest extends AbstractLdapTestUnit {
     
-    private static final String PORT = allocatePort(Server.class);
+    private static final String PORT = TestUtil.getPortNumber(Server.class);
     
-    private static boolean kerberosServerStarted = false;
+    private static boolean portUpdated;
     
     @BeforeClass
-    public static void startServers() throws Exception {
-        assertTrue(
-                   "Server failed to launch",
-                   // run the server in the same process
-                   // set this to false to fork
-                   launchServer(Server.class, true)
-        );
-        
-        kerberosServerStarted = KerberosServiceStarter.startKerberosServer();
+    public static void setUp() throws Exception {
 
+        WSSConfig.init();
+        
         String basedir = System.getProperty("basedir");
         if (basedir == null) {
             basedir = new File(".").getCanonicalPath();
+        } else {
+            basedir += "/..";
+        }
+
+        // System.setProperty("sun.security.krb5.debug", "true");
+        System.setProperty("java.security.auth.login.config", basedir + "/cxf-kerberos/src/test/resources/kerberos/kerberos.jaas");
+        
+        Assert.assertTrue(
+                   "Server failed to launch",
+                   // run the server in the same process
+                   // set this to false to fork
+                   AbstractBusClientServerTestBase.launchServer(Server.class, true)
+        );
+    }
+
+    @Before
+    public void updatePort() throws Exception {
+        if (!portUpdated) {
+            String basedir = System.getProperty("basedir");
+            if (basedir == null) {
+                basedir = new File(".").getCanonicalPath();
+            }
+            
+            // Read in krb5.conf and substitute in the correct port
+            File f = new File(basedir + "/src/test/resources/kerberos/krb5.conf");
+            
+            FileInputStream inputStream = new FileInputStream(f);
+            String content = IOUtils.toString(inputStream, "UTF-8");
+            inputStream.close();
+            content = content.replaceAll("port", "" + super.getKdcServer().getTransports()[0].getPort());
+            
+            File f2 = new File(basedir + "/target/test-classes/kerberos/krb5.conf");
+            FileOutputStream outputStream = new FileOutputStream(f2);
+            IOUtils.write(content, outputStream, "UTF-8");
+            outputStream.close();
+            
+            System.setProperty("java.security.krb5.conf", f2.getPath());
+            
+            portUpdated = true;
         }
     }
     
-    @AfterClass
-    public static void tearDown() throws Exception {
-        if (kerberosServerStarted) {
-            KerberosServiceStarter.stopKerberosServer();
-        }
-    }
-
     @org.junit.Test
     public void testKerberos() throws Exception {
 
@@ -86,8 +169,8 @@ public class JAXRSAuthenticationTest extends AbstractBusClientServerTestBase {
         numberToDouble.setNumber(25);
         
         Response response = client.post(numberToDouble);
-        assertEquals(response.getStatus(), 200);
-        assertEquals(response.readEntity(Number.class).getNumber(), 50);
+        Assert.assertEquals(response.getStatus(), 200);
+        Assert.assertEquals(response.readEntity(Number.class).getNumber(), 50);
     }
     
 }
