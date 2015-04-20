e6d3776a15b4cf032c7db0af2c14788c7adb66ee
Colm O hEigeartaigh <coheigea@apache.org>
2015-04-20 15:25:09 +0100

    Playing around with Apache Kerby

--- apache/cxf/cxf-kerberos-kerby/src/test/java/org/apache/coheigea/cxf/kerberos/authentication/AuthenticationTest.java
+++ apache/cxf/cxf-kerberos-kerby/src/test/java/org/apache/coheigea/cxf/kerberos/authentication/AuthenticationTest.java
@@ -26,23 +26,21 @@ import java.net.URL;
 import javax.xml.namespace.QName;
 import javax.xml.ws.Service;
 
+import org.apache.coheigea.cxf.kerberos.common.KerbyServer;
 import org.apache.commons.io.IOUtils;
 import org.apache.cxf.Bus;
 import org.apache.cxf.bus.spring.SpringBusFactory;
 import org.apache.cxf.testutil.common.AbstractBusClientServerTestBase;
 import org.apache.cxf.testutil.common.TestUtil;
-import org.apache.directory.server.annotations.CreateKdcServer;
-import org.apache.directory.server.annotations.CreateLdapServer;
-import org.apache.directory.server.annotations.CreateTransport;
-import org.apache.directory.server.core.annotations.ApplyLdifFiles;
-import org.apache.directory.server.core.annotations.CreateDS;
-import org.apache.directory.server.core.annotations.CreateIndex;
-import org.apache.directory.server.core.annotations.CreatePartition;
-import org.apache.directory.server.core.integ.AbstractLdapTestUnit;
-import org.apache.directory.server.core.integ.FrameworkRunner;
-import org.apache.directory.server.core.kerberos.KeyDerivationInterceptor;
+import org.apache.kerby.kerberos.kdc.impl.NettyKdcServerImpl;
+import org.apache.kerby.kerberos.kerb.client.KrbClient;
+import org.apache.kerby.kerberos.kerb.server.KdcConfig;
+import org.apache.kerby.kerberos.kerb.server.KdcConfigKey;
+import org.apache.kerby.kerberos.kerb.spec.ticket.ServiceTicket;
+import org.apache.kerby.kerberos.kerb.spec.ticket.TgtTicket;
 import org.apache.wss4j.dom.WSSConfig;
 import org.example.contract.doubleit.DoubleItPortType;
+import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -52,60 +50,21 @@ import org.junit.runner.RunWith;
  * There are two test-cases covered in this class, one that uses a WS-SecurityPolicy 
  * KerberosToken policy, and the other that uses a SpnegoContextToken policy.
  *
- * Both testcases start up a KDC locally using Apache DS. In each case, the service endpoint 
+ * Both testcases start up a KDC locally using Apache Kerby. In each case, the service endpoint 
  * has a TransportBinding policy, with a corresponding EndorsingSupportingToken which is either 
  * a KerberosToken or SpnegoContextToken. The client will obtain a service ticket from the KDC
  * and include it in the security header of the service request.
  */
-
-@RunWith(FrameworkRunner.class)
-
-//Define the DirectoryService
-@CreateDS(name = "KerberosTest-class",
-  enableAccessControl = false,
-  allowAnonAccess = false,
-  enableChangeLog = true,
-  partitions = {
-      @CreatePartition(
-          name = "example",
-          suffix = "dc=example,dc=com",
-          indexes = {
-              @CreateIndex(attribute = "objectClass"),
-              @CreateIndex(attribute = "dc"),
-              @CreateIndex(attribute = "ou")
-          } )
-      },
-  additionalInterceptors = {
-     KeyDerivationInterceptor.class
-  }
-)
-
-@CreateLdapServer(
-  transports = {
-      @CreateTransport(protocol = "LDAP")
-  }
-)
-
-@CreateKdcServer(
-  transports = {
-      // @CreateTransport(protocol = "TCP", address = "127.0.0.1", port=1024)
-      @CreateTransport(protocol = "UDP", address = "127.0.0.1")
-  },
-  primaryRealm = "service.ws.apache.org",
-  kdcPrincipal = "krbtgt/service.ws.apache.org@service.ws.apache.org"
-)
-
-//Inject an file containing entries
-@ApplyLdifFiles("kerberos/kerberos.ldif")
-
-public class AuthenticationTest extends AbstractLdapTestUnit {
+public class AuthenticationTest extends org.junit.Assert {
     
     private static final String NAMESPACE = "http://www.example.org/contract/DoubleIt";
     private static final QName SERVICE_QNAME = new QName(NAMESPACE, "DoubleItService");
     
     private static final String PORT = TestUtil.getPortNumber(Server.class);
+    private static final String KDC_PORT = TestUtil.getPortNumber(Server.class, 2);
+    private static final String KDC_UDP_PORT = TestUtil.getPortNumber(Server.class, 3);
     
-    private static boolean portUpdated;
+    private static KerbyServer kerbyServer;
     
     @BeforeClass
     public static void setUp() throws Exception {
@@ -116,6 +75,8 @@ public class AuthenticationTest extends AbstractLdapTestUnit {
         if (basedir == null) {
             basedir = new File(".").getCanonicalPath();
         }
+        
+        updatePort(basedir);
 
         // System.setProperty("sun.security.krb5.debug", "true");
         System.setProperty("java.security.auth.login.config", basedir + "/src/test/resources/kerberos/kerberos.jaas");
@@ -126,36 +87,87 @@ public class AuthenticationTest extends AbstractLdapTestUnit {
                    // set this to false to fork
                    AbstractBusClientServerTestBase.launchServer(Server.class, true)
         );
+        
+        kerbyServer = new KerbyServer();
+        
+        kerbyServer.setKdcHost("127.0.0.1");
+        kerbyServer.setKdcRealm("service.ws.apache.org");
+        kerbyServer.setKdcTcpPort(Integer.parseInt(KDC_PORT));
+        kerbyServer.setAllowUdp(true);
+        kerbyServer.setKdcUdpPort(Integer.parseInt(KDC_UDP_PORT));
+        
+        // kerbyServer.setInnerKdcImpl(new NettyKdcServerImpl());
+        kerbyServer.init();
+        
+        // Create principals
+        String alice = "alice@service.ws.apache.org";
+        String bob = "bob/service.ws.apache.org@service.ws.apache.org";
+        kerbyServer.createPrincipal(alice, "alice");
+        kerbyServer.createPrincipal(bob, "bob");
+        kerbyServer.createPrincipals("krbtgt/service.ws.apache.org@service.ws.apache.org", "krbtgt");
+        kerbyServer.start();
     }
+    
+    @AfterClass
+    public static void tearDown() {
+    	if (kerbyServer != null) {
+    		kerbyServer.stop();
+    	}
+    }
+    
+    private static void updatePort(String basedir) throws Exception {
 
-    @Before
-    public void updatePort() throws Exception {
-        if (!portUpdated) {
-            String basedir = System.getProperty("basedir");
-            if (basedir == null) {
-                basedir = new File(".").getCanonicalPath();
-            }
-            
-            // Read in krb5.conf and substitute in the correct port
-            File f = new File(basedir + "/src/test/resources/kerberos/krb5.conf");
-            
-            FileInputStream inputStream = new FileInputStream(f);
-            String content = IOUtils.toString(inputStream, "UTF-8");
-            inputStream.close();
-            content = content.replaceAll("port", "" + super.getKdcServer().getTransports()[0].getPort());
-            
-            File f2 = new File(basedir + "/target/test-classes/kerberos/krb5.conf");
-            FileOutputStream outputStream = new FileOutputStream(f2);
-            IOUtils.write(content, outputStream, "UTF-8");
-            outputStream.close();
-            
-            System.setProperty("java.security.krb5.conf", f2.getPath());
-            
-            portUpdated = true;
-        }
+    	// Read in krb5.conf and substitute in the correct port
+    	File f = new File(basedir + "/src/test/resources/kerberos/krb5.conf");
+
+    	FileInputStream inputStream = new FileInputStream(f);
+    	String content = IOUtils.toString(inputStream, "UTF-8");
+    	inputStream.close();
+    	content = content.replaceAll("port", KDC_PORT);
+
+    	File f2 = new File(basedir + "/target/test-classes/kerberos/krb5.conf");
+    	FileOutputStream outputStream = new FileOutputStream(f2);
+    	IOUtils.write(content, outputStream, "UTF-8");
+    	outputStream.close();
+
+    	System.setProperty("java.security.krb5.conf", f2.getPath());
     }
 
     @org.junit.Test
+    @org.junit.Ignore
+    public void unitTest() throws Exception {
+    	KrbClient client = new KrbClient();
+
+    	client.setKdcHost("127.0.0.1");
+    	client.setKdcTcpPort(Integer.parseInt(KDC_PORT));
+    	client.setAllowUdp(true);
+    	client.setKdcUdpPort(Integer.parseInt(KDC_UDP_PORT));
+
+    	client.setTimeout(5);
+    	client.setKdcRealm(kerbyServer.getSetting().getKdcRealm());
+        
+        File testDir = new File(System.getProperty("test.dir", "target"));
+        File testConfDir = new File(testDir, "conf");
+        client.setConfDir(testConfDir);
+        client.init();
+
+        TgtTicket tgt;
+        ServiceTicket tkt;
+
+        try {
+            tgt = client.requestTgtWithPassword("alice@service.ws.apache.org", "alice");
+            assertTrue(tgt != null);
+
+            tkt = client.requestServiceTicketWithTgt(tgt, "bob/service.ws.apache.org@service.ws.apache.org");
+            assertTrue(tkt != null);
+        } catch (Exception e) {
+        	e.printStackTrace();
+            Assert.fail();
+        }
+    }
+    
+    @org.junit.Test
+    // @org.junit.Ignore
     public void testKerberos() throws Exception {
 
         SpringBusFactory bf = new SpringBusFactory();
@@ -164,7 +176,7 @@ public class AuthenticationTest extends AbstractLdapTestUnit {
         Bus bus = bf.createBus(busFile.toString());
         SpringBusFactory.setDefaultBus(bus);
         SpringBusFactory.setThreadDefaultBus(bus);
-        
+    
         URL wsdl = AuthenticationTest.class.getResource("DoubleIt.wsdl");
         Service service = Service.create(wsdl, SERVICE_QNAME);
         QName portQName = new QName(NAMESPACE, "DoubleItKerberosTransportPort");
@@ -173,9 +185,11 @@ public class AuthenticationTest extends AbstractLdapTestUnit {
         TestUtil.updateAddressPort(transportPort, PORT);
         
         doubleIt(transportPort, 25);
+        
     }
     
     @org.junit.Test
+    @org.junit.Ignore
     public void testSpnego() throws Exception {
 
         SpringBusFactory bf = new SpringBusFactory();
---
+++ apache/cxf/cxf-kerberos-kerby/src/test/java/org/apache/coheigea/cxf/kerberos/common/KerbyServer.java
@@ -0,0 +1,66 @@
+/**
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *  
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License. 
+ *  
+ */
+package org.apache.coheigea.cxf.kerberos.common;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.apache.kerby.kerberos.kerb.KrbException;
+import org.apache.kerby.kerberos.kerb.common.EncryptionUtil;
+import org.apache.kerby.kerberos.kerb.identity.KrbIdentity;
+import org.apache.kerby.kerberos.kerb.server.KdcServer;
+import org.apache.kerby.kerberos.kerb.spec.base.EncryptionKey;
+import org.apache.kerby.kerberos.kerb.spec.base.EncryptionType;
+
+/**
+ * A simple extension of the KdcServer which just creates some principals
+ */
+public class KerbyServer extends KdcServer {
+
+    public synchronized void createPrincipal(String principal, String password) {
+        KrbIdentity identity = new KrbIdentity(principal);
+        List<EncryptionType> encTypes = getSetting().getKdcConfig().getEncryptionTypes();
+        List<EncryptionKey> encKeys = null;
+        try {
+            encKeys = EncryptionUtil.generateKeys(fixPrincipal(principal), password, encTypes);
+        } catch (KrbException e) {
+            throw new RuntimeException("Failed to generate encryption keys", e);
+        }
+        identity.addKeys(encKeys);
+        getIdentityService().addIdentity(identity);
+    }
+
+    public void createPrincipals(String ... principals) {
+        String passwd;
+        for (String principal : principals) {
+            passwd = UUID.randomUUID().toString();
+            createPrincipal(fixPrincipal(principal), passwd);
+        }
+    }
+
+    private String fixPrincipal(String principal) {
+        if (! principal.contains("@")) {
+            principal += "@" + getSetting().getKdcRealm();
+        }
+        return principal;
+    }
+
+
+}
