350b1254827b57b0d2352b0038c61a081f9e94ae
Colm O hEigeartaigh <coheigea@apache.org>
2015-10-12 15:06:57 +0100

    Adding authorization tests

---
+++ apache/cxf/cxf-jaxrs-jose/src/test/java/org/apache/coheigea/cxf/jaxrs/jwt/authorization/JWTAuthorizationTest.java
@@ -0,0 +1,210 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.coheigea.cxf.jaxrs.jwt.authorization;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.ws.rs.core.Response;
+
+import org.apache.coheigea.cxf.jaxrs.json.common.Number;
+import org.apache.cxf.jaxrs.client.WebClient;
+import org.apache.cxf.rs.security.jose.JoseType;
+import org.apache.cxf.rs.security.jose.jaxrs.JwtAuthenticationClientFilter;
+import org.apache.cxf.rs.security.jose.jwa.SignatureAlgorithm;
+import org.apache.cxf.rs.security.jose.jws.JwsHeaders;
+import org.apache.cxf.rs.security.jose.jwt.JwtClaims;
+import org.apache.cxf.rs.security.jose.jwt.JwtConstants;
+import org.apache.cxf.rs.security.jose.jwt.JwtToken;
+import org.apache.cxf.testutil.common.AbstractBusClientServerTestBase;
+import org.codehaus.jackson.jaxrs.JacksonJsonProvider;
+import org.junit.BeforeClass;
+
+/**
+ * Test JAX-RS JWT authorization.
+ */
+public class JWTAuthorizationTest extends AbstractBusClientServerTestBase {
+
+    private static final String PORT = allocatePort(Server.class);
+
+    @BeforeClass
+    public static void startServers() throws Exception {
+        assertTrue(
+                   "Server failed to launch",
+                   // run the server in the same process
+                   // set this to false to fork
+                   launchServer(Server.class, true)
+            );
+    }
+
+    @org.junit.Test
+    public void testAuthorizedRequest() throws Exception {
+
+        URL busFile = JWTAuthorizationTest.class.getResource("cxf-client.xml");
+
+        List<Object> providers = new ArrayList<Object>();
+        providers.add(new JacksonJsonProvider());
+        providers.add(new JwtAuthenticationClientFilter());
+        
+        String address = "http://localhost:" + PORT + "/doubleit/services";
+        WebClient client = 
+            WebClient.create(address, providers, busFile.toString());
+        client.type("application/json").accept("application/json");
+        
+        // Create the JWT Token
+        JwtClaims claims = new JwtClaims();
+        claims.setSubject("alice");
+        claims.setIssuer("DoubleItSTSIssuer");
+        claims.setIssuedAt(new Date().getTime() / 1000L);
+        claims.setProperty("role", "boss");
+        
+        JwsHeaders headers = new JwsHeaders();
+        headers.setType(JoseType.JWT);
+        headers.setSignatureAlgorithm(SignatureAlgorithm.RS256);
+        
+        JwtToken token = new JwtToken(headers, claims);
+
+        Map<String, Object> properties = new HashMap<String, Object>();
+        properties.put("rs.security.keystore.type", "jks");
+        properties.put("rs.security.keystore.password", "cspass");
+        properties.put("rs.security.keystore.alias", "myclientkey");
+        properties.put("rs.security.keystore.file", "clientstore.jks");
+        properties.put("rs.security.key.password", "ckpass");
+        properties.put("rs.security.jws.content.signature.algorithm", "RS256");
+
+        // Token must be on the filter properties
+        Map<String, Object> jaxrsProperties = new HashMap<String, Object>();
+        jaxrsProperties.put(JwtConstants.JWT_TOKEN, token);
+        properties.put("jaxrs.filter.properties", jaxrsProperties);
+        
+        WebClient.getConfig(client).getRequestContext().putAll(properties);
+
+        Number numberToDouble = new Number();
+        numberToDouble.setDescription("This is the number to double");
+        numberToDouble.setNumber(25);
+
+        Response response = client.post(numberToDouble);
+        assertEquals(response.getStatus(), 200);
+        assertEquals(response.readEntity(Number.class).getNumber(), 50);
+    }
+    
+    @org.junit.Test
+    public void testNoRole() throws Exception {
+
+        URL busFile = JWTAuthorizationTest.class.getResource("cxf-client.xml");
+
+        List<Object> providers = new ArrayList<Object>();
+        providers.add(new JacksonJsonProvider());
+        providers.add(new JwtAuthenticationClientFilter());
+        
+        String address = "http://localhost:" + PORT + "/doubleit/services";
+        WebClient client = 
+            WebClient.create(address, providers, busFile.toString());
+        client.type("application/json").accept("application/json");
+        
+        // Create the JWT Token
+        JwtClaims claims = new JwtClaims();
+        claims.setSubject("alice");
+        claims.setIssuer("DoubleItSTSIssuer");
+        claims.setIssuedAt(new Date().getTime() / 1000L);
+        
+        JwsHeaders headers = new JwsHeaders();
+        headers.setType(JoseType.JWT);
+        headers.setSignatureAlgorithm(SignatureAlgorithm.RS256);
+        
+        JwtToken token = new JwtToken(headers, claims);
+
+        Map<String, Object> properties = new HashMap<String, Object>();
+        properties.put("rs.security.keystore.type", "jks");
+        properties.put("rs.security.keystore.password", "ispass");
+        properties.put("rs.security.keystore.alias", "imposter");
+        properties.put("rs.security.keystore.file", "imposter.jks");
+        properties.put("rs.security.key.password", "ikpass");
+        properties.put("rs.security.jws.content.signature.algorithm", "RS256");
+
+        // Token must be on the filter properties
+        Map<String, Object> jaxrsProperties = new HashMap<String, Object>();
+        jaxrsProperties.put(JwtConstants.JWT_TOKEN, token);
+        properties.put("jaxrs.filter.properties", jaxrsProperties);
+        
+        WebClient.getConfig(client).getRequestContext().putAll(properties);
+
+        Number numberToDouble = new Number();
+        numberToDouble.setDescription("This is the number to double");
+        numberToDouble.setNumber(25);
+
+        Response response = client.post(numberToDouble);
+        assertNotEquals(response.getStatus(), 200);
+    }
+    
+    @org.junit.Test
+    public void testWrongRole() throws Exception {
+
+        URL busFile = JWTAuthorizationTest.class.getResource("cxf-client.xml");
+
+        List<Object> providers = new ArrayList<Object>();
+        providers.add(new JacksonJsonProvider());
+        providers.add(new JwtAuthenticationClientFilter());
+        
+        String address = "http://localhost:" + PORT + "/doubleit/services";
+        WebClient client = 
+            WebClient.create(address, providers, busFile.toString());
+        client.type("application/json").accept("application/json");
+        
+        // Create the JWT Token
+        JwtClaims claims = new JwtClaims();
+        claims.setSubject("alice");
+        claims.setIssuer("DoubleItSTSIssuer");
+        claims.setIssuedAt(new Date().getTime() / 1000L);
+        claims.setProperty("role", "employee");
+        
+        JwsHeaders headers = new JwsHeaders();
+        headers.setType(JoseType.JWT);
+        headers.setSignatureAlgorithm(SignatureAlgorithm.RS256);
+        
+        JwtToken token = new JwtToken(headers, claims);
+
+        Map<String, Object> properties = new HashMap<String, Object>();
+        properties.put("rs.security.keystore.type", "jks");
+        properties.put("rs.security.keystore.password", "ispass");
+        properties.put("rs.security.keystore.alias", "imposter");
+        properties.put("rs.security.keystore.file", "imposter.jks");
+        properties.put("rs.security.key.password", "ikpass");
+        properties.put("rs.security.jws.content.signature.algorithm", "RS256");
+
+        // Token must be on the filter properties
+        Map<String, Object> jaxrsProperties = new HashMap<String, Object>();
+        jaxrsProperties.put(JwtConstants.JWT_TOKEN, token);
+        properties.put("jaxrs.filter.properties", jaxrsProperties);
+        
+        WebClient.getConfig(client).getRequestContext().putAll(properties);
+
+        Number numberToDouble = new Number();
+        numberToDouble.setDescription("This is the number to double");
+        numberToDouble.setNumber(25);
+
+        Response response = client.post(numberToDouble);
+        assertNotEquals(response.getStatus(), 200);
+    }
+
+}
