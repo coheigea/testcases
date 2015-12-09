0466c40b6cdd24aa9b262aa526fc0e2ba07de829
Colm O hEigeartaigh <coheigea@apache.org>
2015-12-09 11:04:02 +0000

    Removing some tests moved into CXF.

--- apache/cxf/cxf-jaxrs-oauth2.0/src/test/java/org/apache/coheigea/cxf/oauth2/grants/SAMLAuthorizationGrantTest.java
+++ apache/cxf/cxf-jaxrs-oauth2.0/src/test/java/org/apache/coheigea/cxf/oauth2/grants/SAMLAuthorizationGrantTest.java
@@ -37,9 +37,9 @@ import org.apache.wss4j.common.saml.SAMLUtil;
 import org.apache.wss4j.common.saml.SamlAssertionWrapper;
 import org.apache.wss4j.common.saml.bean.AudienceRestrictionBean;
 import org.apache.wss4j.common.saml.bean.ConditionsBean;
-import org.apache.wss4j.common.saml.builder.SAML2Constants;
-import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
 import org.junit.BeforeClass;
+
+import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
 
 /**
  * Test the SAML Authorization grant
@@ -83,144 +83,6 @@ public class SAMLAuthorizationGrantTest extends AbstractBusClientServerTestBase 
         ClientAccessToken accessToken = response.readEntity(ClientAccessToken.class);
         assertNotNull(accessToken.getTokenKey());
         assertNotNull(accessToken.getRefreshToken());
-    }
-    
-    @org.junit.Test
-    public void testSAML11() throws Exception {
-        URL busFile = SAMLAuthorizationGrantTest.class.getResource("cxf-client.xml");
-        
-        List<Object> providers = new ArrayList<Object>();
-        providers.add(new JacksonJsonProvider());
-        
-        String address = "https://localhost:" + PORT + "/services/";
-        WebClient client = WebClient.create(address, providers, "alice", "security", busFile.toString());
-        
-        // Create the SAML Assertion
-        String assertion = createToken(address + "token", false, true);
-
-        // Get Access Token
-        client.type("application/x-www-form-urlencoded").accept("application/json");
-        client.path("token");
-        
-        Form form = new Form();
-        form.param("grant_type", "urn:ietf:params:oauth:grant-type:saml2-bearer");
-        form.param("assertion", Base64UrlUtility.encode(assertion));
-        form.param("client_id", "consumer-id");
-        
-        try {
-            Response response = client.post(form);
-            response.readEntity(ClientAccessToken.class);
-            fail("Failure expected on a SAML 1.1 assertion");
-        } catch (Exception ex) {
-            // expected
-        }
-    }
-    
-    @org.junit.Test
-    public void testSAMLAudRestr() throws Exception {
-        URL busFile = SAMLAuthorizationGrantTest.class.getResource("cxf-client.xml");
-        
-        List<Object> providers = new ArrayList<Object>();
-        providers.add(new JacksonJsonProvider());
-        
-        String address = "https://localhost:" + PORT + "/services/";
-        WebClient client = WebClient.create(address, providers, "alice", "security", busFile.toString());
-        
-        // Create the SAML Assertion
-        String assertion = createToken(address + "token2", true, true);
-
-        // Get Access Token
-        client.type("application/x-www-form-urlencoded").accept("application/json");
-        client.path("token");
-        
-        Form form = new Form();
-        form.param("grant_type", "urn:ietf:params:oauth:grant-type:saml2-bearer");
-        form.param("assertion", Base64UrlUtility.encode(assertion));
-        form.param("client_id", "consumer-id");
-        
-        try {
-            Response response = client.post(form);
-            response.readEntity(ClientAccessToken.class);
-            fail("Failure expected on a bad audience restriction");
-        } catch (Exception ex) {
-            // expected
-        }
-    }
-    
-    @org.junit.Test
-    public void testSAMLUnsigned() throws Exception {
-        URL busFile = SAMLAuthorizationGrantTest.class.getResource("cxf-client.xml");
-        
-        List<Object> providers = new ArrayList<Object>();
-        providers.add(new JacksonJsonProvider());
-        
-        String address = "https://localhost:" + PORT + "/services/";
-        WebClient client = WebClient.create(address, providers, "alice", "security", busFile.toString());
-        
-        // Create the SAML Assertion
-        String assertion = createToken(address + "token", true, false);
-
-        // Get Access Token
-        client.type("application/x-www-form-urlencoded").accept("application/json");
-        client.path("token");
-        
-        Form form = new Form();
-        form.param("grant_type", "urn:ietf:params:oauth:grant-type:saml2-bearer");
-        form.param("assertion", Base64UrlUtility.encode(assertion));
-        form.param("client_id", "consumer-id");
-        
-        try {
-            Response response = client.post(form);
-            response.readEntity(ClientAccessToken.class);
-            fail("Failure expected on an unsigned assertion");
-        } catch (Exception ex) {
-            // expected
-        }
-    }
-    
-    @org.junit.Test
-    public void testSAMLHolderOfKey() throws Exception {
-        URL busFile = SAMLAuthorizationGrantTest.class.getResource("cxf-hok-client.xml");
-        
-        List<Object> providers = new ArrayList<Object>();
-        providers.add(new JacksonJsonProvider());
-        
-        String address = "https://localhost:" + PORT + "/services/";
-        WebClient client = WebClient.create(address, providers, "alice", "security", busFile.toString());
-        
-        // Create the SAML Assertion
-        SamlCallbackHandler samlCallbackHandler = new SamlCallbackHandler();
-        samlCallbackHandler.setConfirmationMethod(SAML2Constants.CONF_HOLDER_KEY);
-        samlCallbackHandler.setSignAssertion(true);
-        
-        ConditionsBean conditions = new ConditionsBean();
-        AudienceRestrictionBean audienceRestriction = new AudienceRestrictionBean();
-        audienceRestriction.setAudienceURIs(Collections.singletonList(address + "token"));
-        conditions.setAudienceRestrictions(Collections.singletonList(audienceRestriction));
-        samlCallbackHandler.setConditions(conditions);
-        
-        SAMLCallback samlCallback = new SAMLCallback();
-        SAMLUtil.doSAMLCallback(samlCallbackHandler, samlCallback);
-
-        SamlAssertionWrapper samlAssertion = new SamlAssertionWrapper(samlCallback);
-        String assertion = samlAssertion.assertionToString();
-
-        // Get Access Token
-        client.type("application/x-www-form-urlencoded").accept("application/json");
-        client.path("token");
-        
-        Form form = new Form();
-        form.param("grant_type", "urn:ietf:params:oauth:grant-type:saml2-bearer");
-        form.param("assertion", Base64UrlUtility.encode(assertion));
-        form.param("client_id", "consumer-id");
-        
-        try {
-            Response response = client.post(form);
-            response.readEntity(ClientAccessToken.class);
-            fail("Failure expected on an incorrect subject confirmation method");
-        } catch (Exception ex) {
-            // expected
-        }
     }
     
     private String createToken(String audRestr, boolean saml2, boolean sign) throws WSSecurityException {
