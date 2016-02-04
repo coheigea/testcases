d2c28383c05723473fbf96f59bd1858650512f75
Colm O hEigeartaigh <coheigea@apache.org>
2016-02-04 16:42:46 +0000

    Adding audience test

--- apache/cxf/cxf-jaxrs-oidc/src/test/java/org/apache/coheigea/cxf/oidc/flows/AuthorizationCodeFlowTest.java
+++ apache/cxf/cxf-jaxrs-oidc/src/test/java/org/apache/coheigea/cxf/oidc/flows/AuthorizationCodeFlowTest.java
@@ -111,7 +111,7 @@ public class AuthorizationCodeFlowTest extends AbstractBusClientServerTestBase {
             org.apache.cxf.message.Message.MAINTAIN_SESSION, Boolean.TRUE);
         
         // Get Authorization Code
-        String code = getAuthorizationCode(client, "read_balance", null, null);
+        String code = getAuthorizationCode(client, "read_balance", null, null, "consumer-id");
         assertNotNull(code);
         
         // Now get the access token
@@ -142,7 +142,7 @@ public class AuthorizationCodeFlowTest extends AbstractBusClientServerTestBase {
             org.apache.cxf.message.Message.MAINTAIN_SESSION, Boolean.TRUE);
         
         // Get Authorization Code
-        String code = getAuthorizationCode(client, "openid", "123456789", null);
+        String code = getAuthorizationCode(client, "openid", "123456789", null, "consumer-id");
         assertNotNull(code);
         
         // Now get the access token
@@ -254,7 +254,7 @@ public class AuthorizationCodeFlowTest extends AbstractBusClientServerTestBase {
             org.apache.cxf.message.Message.MAINTAIN_SESSION, Boolean.TRUE);
         
         // Get Authorization Code
-        String code = getAuthorizationCode(client, "openid", null, "123456789");
+        String code = getAuthorizationCode(client, "openid", null, "123456789", "consumer-id");
         assertNotNull(code);
         
         // Now get the access token
@@ -272,15 +272,44 @@ public class AuthorizationCodeFlowTest extends AbstractBusClientServerTestBase {
         validateIdToken(idToken, null);
     }
     
+    @org.junit.Test
+    public void testAuthorizationCodeFlowWithAudience() throws Exception {
+        URL busFile = AuthorizationCodeFlowTest.class.getResource("cxf-client.xml");
+        
+        List<Object> providers = new ArrayList<Object>();
+        providers.add(new JacksonJsonProvider());
+        
+        String address = "https://localhost:" + PORT + "/services/";
+        WebClient client = WebClient.create(address, providers, "alice", "security", busFile.toString());
+        // Save the Cookie for the second request...
+        WebClient.getConfig(client).getRequestContext().put(
+            org.apache.cxf.message.Message.MAINTAIN_SESSION, Boolean.TRUE);
+        
+        // Get Authorization Code
+        String code = getAuthorizationCode(client, "openid", null, null, "consumer-id-aud");
+        assertNotNull(code);
+        
+        // Now get the access token
+        client = WebClient.create(address, providers, "consumer-id-aud", "this-is-a-secret", busFile.toString());
+        // Save the Cookie for the second request...
+        WebClient.getConfig(client).getRequestContext().put(
+            org.apache.cxf.message.Message.MAINTAIN_SESSION, Boolean.TRUE);
+        
+        String audience = "https://localhost:12345/bankservice/partners/balance";
+        ClientAccessToken accessToken = 
+            getAccessTokenWithAuthorizationCode(client, code, "consumer-id-aud", audience);
+        assertNotNull(accessToken.getTokenKey());
+    }
+    
     private String getAuthorizationCode(WebClient client, String scope) {
-        return getAuthorizationCode(client, scope, null, null);
+        return getAuthorizationCode(client, scope, null, null, "consumer-id");
     }
     
     private String getAuthorizationCode(WebClient client, String scope,
-                                        String nonce, String state) {
+                                        String nonce, String state, String consumerId) {
         // Make initial authorization request
         client.type("application/json").accept("application/json");
-        client.query("client_id", "consumer-id");
+        client.query("client_id", consumerId);
         client.query("redirect_uri", "http://www.blah.apache.org");
         client.query("response_type", "code");
         if (scope != null) {
@@ -326,13 +355,23 @@ public class AuthorizationCodeFlowTest extends AbstractBusClientServerTestBase {
     }
     
     private ClientAccessToken getAccessTokenWithAuthorizationCode(WebClient client, String code) {
+        return getAccessTokenWithAuthorizationCode(client, code, "consumer-id", null);
+    }
+    
+    private ClientAccessToken getAccessTokenWithAuthorizationCode(WebClient client, 
+                                                                  String code,
+                                                                  String consumerId,
+                                                                  String audience) {
         client.type("application/x-www-form-urlencoded").accept("application/json");
         client.path("token");
         
         Form form = new Form();
         form.param("grant_type", "authorization_code");
         form.param("code", code);
-        form.param("client_id", "consumer-id");
+        form.param("client_id", consumerId);
+        if (audience != null) {
+            form.param("audience", audience);
+        }
         Response response = client.post(form);
         
         return response.readEntity(ClientAccessToken.class);
