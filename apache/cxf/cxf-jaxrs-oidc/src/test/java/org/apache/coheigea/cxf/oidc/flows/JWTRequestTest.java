177d7a0b954e3c5cf6600e002ad03217c04a146b
Colm O hEigeartaigh <coheigea@apache.org>
2016-02-17 16:31:28 +0000

    Adding JWTRequest tests

---
+++ apache/cxf/cxf-jaxrs-oidc/src/test/java/org/apache/coheigea/cxf/oidc/flows/JWTRequestTest.java
@@ -0,0 +1,269 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.coheigea.cxf.oidc.flows;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
+import javax.ws.rs.client.ResponseProcessingException;
+import javax.ws.rs.core.Form;
+import javax.ws.rs.core.Response;
+
+import org.apache.coheigea.cxf.oidc.provider.OIDCProviderServer;
+import org.apache.cxf.jaxrs.client.WebClient;
+import org.apache.cxf.rs.security.jose.jws.JwsHeaders;
+import org.apache.cxf.rs.security.jose.jws.JwsJwtCompactProducer;
+import org.apache.cxf.rs.security.jose.jwt.JwtClaims;
+import org.apache.cxf.rs.security.jose.jwt.JwtToken;
+import org.apache.cxf.rs.security.oauth2.common.OAuthAuthorizationData;
+import org.apache.cxf.testutil.common.AbstractBusClientServerTestBase;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+
+import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
+
+/**
+ * Some unit tests to test sending the OIDC request as a JWT
+ */
+public class JWTRequestTest extends AbstractBusClientServerTestBase {
+    
+    static final String PORT = allocatePort(OIDCProviderServer.class);
+    @BeforeClass
+    public static void startServers() throws Exception {
+        assertTrue(
+                "Server failed to launch",
+                // run the server in the same process
+                // set this to false to fork
+                launchServer(OIDCProviderServer.class, true)
+        );
+    }
+    
+    @org.junit.Test
+    public void testAuthorizationCodeFlowUnsignedJWT() throws Exception {
+        URL busFile = JWTRequestTest.class.getResource("cxf-client.xml");
+        
+        List<Object> providers = new ArrayList<Object>();
+        providers.add(new JacksonJsonProvider());
+        
+        String address = "https://localhost:" + PORT + "/unsignedjwtservices/";
+        WebClient client = WebClient.create(address, providers, "alice", "security", busFile.toString());
+        // Save the Cookie for the second request...
+        WebClient.getConfig(client).getRequestContext().put(
+            org.apache.cxf.message.Message.MAINTAIN_SESSION, Boolean.TRUE);
+        
+        JwtClaims claims = new JwtClaims();
+        claims.setIssuer("consumer-id");
+        claims.setIssuedAt(new Date().getTime() / 1000L);
+        claims.setAudiences(
+            Collections.singletonList("https://localhost:" + PORT + "/unsignedjwtservices/"));
+        
+        JwsHeaders headers = new JwsHeaders();
+        headers.setAlgorithm("none");
+        
+        JwtToken token = new JwtToken(headers, claims);
+        
+        JwsJwtCompactProducer jws = new JwsJwtCompactProducer(token);
+        String request = jws.getSignedEncodedJws();
+        
+        // Get Authorization Code
+        String code = getAuthorizationCode(client, "openid", request);
+        assertNotNull(code);
+    }
+    
+    @org.junit.Test
+    public void testAuthorizationCodeFlowUnsignedJWTWithState() throws Exception {
+        URL busFile = JWTRequestTest.class.getResource("cxf-client.xml");
+        
+        List<Object> providers = new ArrayList<Object>();
+        providers.add(new JacksonJsonProvider());
+        
+        String address = "https://localhost:" + PORT + "/unsignedjwtservices/";
+        WebClient client = WebClient.create(address, providers, "alice", "security", busFile.toString());
+        // Save the Cookie for the second request...
+        WebClient.getConfig(client).getRequestContext().put(
+            org.apache.cxf.message.Message.MAINTAIN_SESSION, Boolean.TRUE);
+        
+        JwtClaims claims = new JwtClaims();
+        claims.setIssuer("consumer-id");
+        claims.setIssuedAt(new Date().getTime() / 1000L);
+        claims.setAudiences(
+            Collections.singletonList("https://localhost:" + PORT + "/unsignedjwtservices/"));
+        
+        JwsHeaders headers = new JwsHeaders();
+        headers.setAlgorithm("none");
+        
+        JwtToken token = new JwtToken(headers, claims);
+        
+        JwsJwtCompactProducer jws = new JwsJwtCompactProducer(token);
+        String request = jws.getSignedEncodedJws();
+        
+        // Get Authorization Code
+        String code = getAuthorizationCode(client, "openid", null, "123456789", 
+                                           "consumer-id", request);
+        assertNotNull(code);
+    }
+    
+    @org.junit.Test
+    public void testNonmatchingResponseType() throws Exception {
+        URL busFile = JWTRequestTest.class.getResource("cxf-client.xml");
+        
+        List<Object> providers = new ArrayList<Object>();
+        providers.add(new JacksonJsonProvider());
+        
+        String address = "https://localhost:" + PORT + "/unsignedjwtservices/";
+        WebClient client = WebClient.create(address, providers, "alice", "security", busFile.toString());
+        // Save the Cookie for the second request...
+        WebClient.getConfig(client).getRequestContext().put(
+            org.apache.cxf.message.Message.MAINTAIN_SESSION, Boolean.TRUE);
+        
+        JwtClaims claims = new JwtClaims();
+        claims.setIssuer("consumer-id");
+        claims.setIssuedAt(new Date().getTime() / 1000L);
+        claims.setAudiences(
+            Collections.singletonList("https://localhost:" + PORT + "/unsignedjwtservices/"));
+        //claims.setProperty("client_id", "consumer-id");
+        claims.setProperty("response_type", "token");
+        
+        JwsHeaders headers = new JwsHeaders();
+        headers.setAlgorithm("none");
+        
+        JwtToken token = new JwtToken(headers, claims);
+        
+        JwsJwtCompactProducer jws = new JwsJwtCompactProducer(token);
+        String request = jws.getSignedEncodedJws();
+        
+        // Get Authorization Code
+        try {
+            getAuthorizationCode(client, "openid", request);
+            fail("Failure expected on a non-matching response_type");
+        } catch (ResponseProcessingException ex) {
+            // expected
+        }
+    }
+    
+    @org.junit.Test
+    public void testNonmatchingClientId() throws Exception {
+        URL busFile = JWTRequestTest.class.getResource("cxf-client.xml");
+        
+        List<Object> providers = new ArrayList<Object>();
+        providers.add(new JacksonJsonProvider());
+        
+        String address = "https://localhost:" + PORT + "/unsignedjwtservices/";
+        WebClient client = WebClient.create(address, providers, "alice", "security", busFile.toString());
+        // Save the Cookie for the second request...
+        WebClient.getConfig(client).getRequestContext().put(
+            org.apache.cxf.message.Message.MAINTAIN_SESSION, Boolean.TRUE);
+        
+        JwtClaims claims = new JwtClaims();
+        claims.setIssuer("consumer-id");
+        claims.setIssuedAt(new Date().getTime() / 1000L);
+        claims.setAudiences(
+            Collections.singletonList("https://localhost:" + PORT + "/unsignedjwtservices/"));
+        claims.setProperty("client_id", "consumer-id2");
+        
+        JwsHeaders headers = new JwsHeaders();
+        headers.setAlgorithm("none");
+        
+        JwtToken token = new JwtToken(headers, claims);
+        
+        JwsJwtCompactProducer jws = new JwsJwtCompactProducer(token);
+        String request = jws.getSignedEncodedJws();
+        
+        // Get Authorization Code
+        try {
+            getAuthorizationCode(client, "openid", request);
+            fail("Failure expected on a non-matching client_id");
+        } catch (ResponseProcessingException ex) {
+            // expected
+        }
+    }
+    
+    private String getAuthorizationCode(WebClient client, String scope, String request) {
+        return getAuthorizationCode(client, scope, null, null, "consumer-id", request);
+    }
+    
+    private String getAuthorizationCode(WebClient client, String scope,
+                                        String nonce, String state, String consumerId,
+                                        String request) {
+        // Make initial authorization request
+        client.type("application/json").accept("application/json");
+        
+        client.query("client_id", consumerId);
+        client.query("redirect_uri", "http://www.blah.apache.org");
+        client.query("response_type", "code");
+        if (scope != null) {
+            client.query("scope", scope);
+        }
+        if (nonce != null) {
+            client.query("nonce", nonce);
+        }
+        if (state != null) {
+            client.query("state", state);
+        }
+        if (request != null) {
+            client.query("request", request);
+        }
+        
+        client.path("authorize/");
+        Response response = client.get();
+        
+        OAuthAuthorizationData authzData = response.readEntity(OAuthAuthorizationData.class);
+        
+        // Now call "decision" to get the authorization code grant
+        client.path("decision");
+        client.type("application/x-www-form-urlencoded");
+        
+        Form form = new Form();
+        form.param("session_authenticity_token", authzData.getAuthenticityToken());
+        form.param("client_id", authzData.getClientId());
+        form.param("redirect_uri", authzData.getRedirectUri());
+        if (authzData.getNonce() != null) {
+            form.param("nonce", authzData.getNonce());
+        }
+        if (authzData.getProposedScope() != null) {
+            form.param("scope", authzData.getProposedScope());
+        }
+        if (authzData.getState() != null) {
+            form.param("state", authzData.getState());
+        }
+        form.param("oauthDecision", "allow");
+        
+        response = client.post(form);
+        String location = response.getHeaderString("Location"); 
+        if (state != null) {
+            Assert.assertTrue(location.contains("state=" + state));
+        }
+        
+        return getSubstring(location, "code");
+    }
+    
+    private String getSubstring(String parentString, String substringName) {
+        String foundString = 
+            parentString.substring(parentString.indexOf(substringName + "=") + (substringName + "=").length());
+        int ampersandIndex = foundString.indexOf('&');
+        if (ampersandIndex < 1) {
+            ampersandIndex = foundString.length();
+        }
+        return foundString.substring(0, ampersandIndex);
+    }
+    
+}
