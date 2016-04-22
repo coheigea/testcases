891dacc2c4b28d2080e95af92fa6b858e6e2452b
Colm O hEigeartaigh <coheigea@apache.org>
2016-04-22 17:37:45 +0100

    Adding HDFS test

---
+++ apache/bigdata/hive/src/test/java/org/apache/coheigea/bigdata/hdfs/HDFSTest.java
@@ -0,0 +1,61 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.coheigea.bigdata.hdfs;
+
+import java.io.File;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hdfs.MiniDFSCluster;
+
+public class HDFSTest {
+    
+    @org.junit.Test
+    public void simpleTest() throws Exception {
+        
+        File baseDir = new File("./target/hdfs/").getAbsoluteFile();
+        // FileUtil.fullyDelete(baseDir);
+        Configuration conf = new Configuration();
+        conf.set(MiniDFSCluster.HDFS_MINIDFS_BASEDIR, baseDir.getAbsolutePath());
+        MiniDFSCluster.Builder builder = new MiniDFSCluster.Builder(conf);
+        MiniDFSCluster hdfsCluster = builder.build();
+        String hdfsURI = "hdfs://localhost:"+ hdfsCluster.getNameNodePort() + "/";
+        FileSystem fileSystem = hdfsCluster.getFileSystem();
+        
+        // Write a file
+        final Path file = new Path(new File("target/hdfs/data-file").getAbsolutePath());
+        FSDataOutputStream out = fileSystem.create(file);
+        for (int i = 0; i < 1024; ++i) {
+            out.write(("data" + i).getBytes("UTF-8"));
+            out.flush();
+        }
+        out.close();
+        
+        // Read the file
+        FSDataInputStream in = fileSystem.open(file);
+        
+        // Print it out
+        IOUtils.copy(in, System.out);
+    }
+    
+    // TODO teardown
+}
