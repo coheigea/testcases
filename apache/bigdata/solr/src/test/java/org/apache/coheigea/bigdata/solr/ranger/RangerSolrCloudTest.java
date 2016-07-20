6533faa4060fbc138b4443991404d8809a173e16
Colm O hEigeartaigh <coheigea@apache.org>
2016-07-20 17:22:17 +0100

    Adding initial Ranger SolrCloud test

--- apache/bigdata/solr/src/test/java/org/apache/coheigea/bigdata/solr/ranger/RangerSolrCloudTest.java
+++ apache/bigdata/solr/src/test/java/org/apache/coheigea/bigdata/solr/ranger/RangerSolrCloudTest.java
@@ -15,34 +15,46 @@
  * limitations under the License.
  */
 
-package org.apache.coheigea.bigdata.solr;
+package org.apache.coheigea.bigdata.solr.ranger;
 
 import java.io.File;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.security.PrivilegedExceptionAction;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.hadoop.security.UserGroupInformation;
+import org.apache.solr.client.solrj.SolrRequest;
 import org.apache.solr.client.solrj.embedded.JettyConfig;
 import org.apache.solr.client.solrj.embedded.JettySolrRunner;
 import org.apache.solr.client.solrj.impl.CloudSolrClient;
+import org.apache.solr.client.solrj.request.QueryRequest;
 import org.apache.solr.client.solrj.response.QueryResponse;
 import org.apache.solr.cloud.MiniSolrCloudCluster;
 import org.apache.solr.common.SolrDocument;
 import org.apache.solr.common.SolrDocumentList;
 import org.apache.solr.common.SolrInputDocument;
+import org.apache.solr.common.cloud.ZkStateReader;
 import org.apache.solr.common.params.ModifiableSolrParams;
+import org.apache.zookeeper.CreateMode;
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
- * Simple test that starts SolrCloud, adds a new document and queries it. 
+ * Simple test that starts SolrCloud, adds a new document and queries it. It also plugs in the RangerSolrAuthorizer with the following policies:
+ * 
+ * a) "bob" has all privileges on the "docs" collection
+ * b) "alice" and the "IT" group can only query the "docs" collection
+ * c) The "Legal" group can only query the "docs" collection from the IP 127.0.0.*
+ * 
  */
-public class SolrCloudTest extends org.junit.Assert {
+@org.junit.Ignore
+public class RangerSolrCloudTest extends org.junit.Assert {
     
     private static MiniSolrCloudCluster server;
     private static Path tempDir;
@@ -57,8 +69,21 @@ public class SolrCloudTest extends org.junit.Assert {
         tempDir = Files.createTempDirectory("solrcloud");
         server = new MiniSolrCloudCluster(2, tempDir, solrConfig, jettyConfig.build());
         
-        JettySolrRunner startedServer = server.startJettySolrRunner();
-        assertTrue(startedServer.isRunning());
+        // Insert the RangerSolrAuthorizer + BasicAuthPlugin
+        try (ZkStateReader zkStateReader = new ZkStateReader( server.getZkServer().getZkAddress(),
+                                                              10000, 10000)) {
+            zkStateReader.getZkClient().delete(ZkStateReader.SOLR_SECURITY_CONF_PATH, 0, true);
+            String rangerAuthorizer = "org.apache.ranger.authorization.solr.authorizer.RangerSolrAuthorizer";
+            String basicAuthAuthenticator = "solr.BasicAuthPlugin";
+            zkStateReader.getZkClient().create(ZkStateReader.SOLR_SECURITY_CONF_PATH,
+                                               ("{"
+                                               + "\"authentication\":{\"blockUnknown\":false,\"class\":\"" + basicAuthAuthenticator + "\","
+                                               + "\"credentials\":{\"bob\":\"IV0EHq1OnNrj6gvRCwvFwTrZ1+z1oBbnQdiVC3otuq0= Ndd7LKvVBAaZIF0QAVi1ekCfAJXr1GGfLtRUXhgrF8c=\"}},"
+                                               + "\"authorization\":{\"class\":\"" + rangerAuthorizer + "\"}"
+                                               + "}"
+                                               ).getBytes(Charset.defaultCharset()),
+                                               CreateMode.PERSISTENT, true);
+        }
         
         String configName = "core1Config";
         File configDir = Paths.get("src/test/resources/solrcloud").toFile();
@@ -69,6 +94,10 @@ public class SolrCloudTest extends org.junit.Assert {
         collectionProperties.put("schema", "schema.xml");
         
         server.createCollection("docs", 1, 1, configName, collectionProperties);
+        
+        JettySolrRunner startedServer = server.startJettySolrRunner();
+        assertTrue(startedServer.isRunning());
+        
     }
     
     @AfterClass
@@ -81,7 +110,8 @@ public class SolrCloudTest extends org.junit.Assert {
 
     @Test
     public void testAddAndQuery() throws Exception {
-        CloudSolrClient cloudSolrClient = server.getSolrClient();
+        
+        final CloudSolrClient cloudSolrClient = server.getSolrClient();
         
         cloudSolrClient.setDefaultCollection("docs");
         
@@ -96,13 +126,21 @@ public class SolrCloudTest extends org.junit.Assert {
         ModifiableSolrParams params = new ModifiableSolrParams();
         // Test it's uploaded
         params.set("q", "*");
-        QueryResponse qResp = cloudSolrClient.query(params);
 
-        SolrDocumentList foundDocs = qResp.getResults();
-        Assert.assertEquals(1, foundDocs.getNumFound());
-        
-        SolrDocument foundDoc = foundDocs.get(0);
-        Assert.assertEquals("Title of Doc", foundDoc.getFieldValue("title"));
+        SolrRequest solrRequest = new QueryRequest(params);
+        solrRequest.setBasicAuthCredentials("bob", "SolrRocks"); 
+
+        // TODO parse response
+        cloudSolrClient.request(solrRequest);
+        /*
+                QueryResponse qResp = cloudSolrClient.query(params);
+
+                SolrDocumentList foundDocs = qResp.getResults();
+                Assert.assertEquals(1, foundDocs.getNumFound());
+
+                SolrDocument foundDoc = foundDocs.get(0);
+                Assert.assertEquals("Title of Doc", foundDoc.getFieldValue("title"));
+         */
     }
     
 
