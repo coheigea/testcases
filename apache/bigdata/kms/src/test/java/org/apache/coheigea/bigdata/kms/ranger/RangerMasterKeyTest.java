a6172e81be37e919316da178b4fdcac031abe0b5
Colm O hEigeartaigh <coheigea@apache.org>
2016-08-04 12:11:48 +0100

    Added RangerMasterKeyTest

--- apache/bigdata/kms/src/test/java/org/apache/coheigea/bigdata/kms/ranger/RangerMasterKeyTest.java
+++ apache/bigdata/kms/src/test/java/org/apache/coheigea/bigdata/kms/ranger/RangerMasterKeyTest.java
@@ -17,30 +17,27 @@
 
 package org.apache.coheigea.bigdata.kms.ranger;
 
-import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Arrays;
 import java.util.Properties;
 
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.crypto.key.KeyProvider.KeyVersion;
-import org.apache.hadoop.crypto.key.KeyProvider.Options;
+import org.apache.hadoop.crypto.key.RangerKMSDB;
 import org.apache.hadoop.crypto.key.RangerKeyStoreProvider;
+import org.apache.hadoop.crypto.key.RangerMasterKey;
 import org.apache.hadoop.crypto.key.kms.server.KMSConfiguration;
+import org.apache.ranger.kms.dao.DaoManager;
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 
 /**
- * A test for the RangerKeyStoreProvider, which is an implementation of the Hadoop KeyProvider interface, which stores keys in a database.
- * Apache Derby is used to create the relevant tables to store the keys in for this test.
+ * A test for the RangerMasterKey.
  */
-public class RangerKeyStoreProviderTest {
+public class RangerMasterKeyTest {
     
     private static Connection conn;
     
@@ -86,74 +83,26 @@ public class RangerKeyStoreProviderTest {
             // expected
         }
     }
-
-    @org.junit.Test
-    public void testCreateDeleteKey() throws Throwable {
-        
-        Path configDir = Paths.get("src/test/resources/kms");
-        System.setProperty(KMSConfiguration.KMS_CONFIG_DIR, configDir.toFile().getAbsolutePath());
-        
-        Configuration conf = new Configuration();
-        RangerKeyStoreProvider keyProvider = new RangerKeyStoreProvider(conf);
-        
-        // Create a key
-        Options options = new Options(conf);
-        options.setBitLength(128);
-        options.setCipher("AES");
-        KeyVersion keyVersion = keyProvider.createKey("newkey1", options);
-        Assert.assertEquals("newkey1", keyVersion.getName());
-        Assert.assertEquals(128 / 8, keyVersion.getMaterial().length);
-        Assert.assertEquals("newkey1@0", keyVersion.getVersionName());
-        
-        keyProvider.flush();
-        Assert.assertEquals(1, keyProvider.getKeys().size());
-        keyProvider.deleteKey("newkey1");
-        
-        keyProvider.flush();
-        Assert.assertEquals(0, keyProvider.getKeys().size());
-        
-        // Try to delete a key that isn't there
-        try {
-            keyProvider.deleteKey("newkey2");
-            Assert.fail("Failure expected on trying to delete an unknown key");
-        } catch (IOException ex) {
-            // expected
-        }
-    }
     
     @org.junit.Test
-    public void testRolloverKey() throws Throwable {
+    public void testRangerMasterKey() throws Throwable {
         
         Path configDir = Paths.get("src/test/resources/kms");
         System.setProperty(KMSConfiguration.KMS_CONFIG_DIR, configDir.toFile().getAbsolutePath());
+
+        RangerKMSDB rangerkmsDb = new RangerKMSDB(RangerKeyStoreProvider.getDBKSConf());     
+        DaoManager daoManager = rangerkmsDb.getDaoManager();
         
-        Configuration conf = new Configuration();
-        RangerKeyStoreProvider keyProvider = new RangerKeyStoreProvider(conf);
+        RangerMasterKey rangerMasterKey = new RangerMasterKey(daoManager);
+        Assert.assertTrue(rangerMasterKey.generateMasterKey("NewPassword"));
+        Assert.assertNotNull(rangerMasterKey.getMasterKey("NewPassword"));
         
-        // Create a key
-        Options options = new Options(conf);
-        options.setBitLength(192);
-        options.setCipher("AES");
-        KeyVersion keyVersion = keyProvider.createKey("newkey1", options);
-        Assert.assertEquals("newkey1", keyVersion.getName());
-        Assert.assertEquals(192 / 8, keyVersion.getMaterial().length);
-        Assert.assertEquals("newkey1@0", keyVersion.getVersionName());
-        
-        keyProvider.flush();
-        
-        // Rollover a new key
-        byte[] oldKey = keyVersion.getMaterial();
-        keyVersion = keyProvider.rollNewVersion("newkey1");
-        Assert.assertEquals("newkey1", keyVersion.getName());
-        Assert.assertEquals(192 / 8, keyVersion.getMaterial().length);
-        Assert.assertEquals("newkey1@1", keyVersion.getVersionName());
-        Assert.assertFalse(Arrays.equals(oldKey, keyVersion.getMaterial()));
-        
-        keyProvider.deleteKey("newkey1");
-        
-        keyProvider.flush();
-        Assert.assertEquals(0, keyProvider.getKeys().size());
-        
+        try {
+            rangerMasterKey.getMasterKey("badpass");
+            Assert.fail("Failure expected on retrieving a key with the wrong password");
+        } catch (Throwable t) {
+            // expected
+        }
     }
     
 }
