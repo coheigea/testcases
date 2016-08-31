3f89966753b4a543f2518142264a81c33d5baaae
Colm O hEigeartaigh <coheigea@apache.org>
2016-08-31 16:57:09 +0100

    Got test working...

--- apache/bigdata/kafka/src/test/java/org/apache/coheigea/bigdata/kafka/KafkaTest.java
+++ apache/bigdata/kafka/src/test/java/org/apache/coheigea/bigdata/kafka/KafkaTest.java
@@ -26,18 +26,20 @@ import org.apache.curator.test.TestingServer;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.junit.Assert;
 
 import kafka.admin.AdminUtils;
 import kafka.admin.RackAwareMode;
-import kafka.javaapi.producer.Producer;
-import kafka.producer.KeyedMessage;
-import kafka.producer.ProducerConfig;
 import kafka.server.KafkaConfig;
 import kafka.server.KafkaServerStartable;
 import kafka.utils.ZKStringSerializer$;
 import kafka.utils.ZkUtils;
 
 /**
+ * A simple test that starts a Kafka broker, creates a "test" topic, sends a message to it and consumes it.
  */
 public class KafkaTest {
     
@@ -47,100 +49,84 @@ public class KafkaTest {
     @org.junit.BeforeClass
     public static void setup() throws Exception {
         zkServer = new TestingServer();
-        System.out.println("CONNECT: " + zkServer.getConnectString());
-        KafkaConfig config = getKafkaConfig(zkServer.getConnectString());
-        kafkaServer = new KafkaServerStartable(config);
-        kafkaServer.startup();
-    }
-    
-    @org.junit.Test
-    public void testKafka() throws Exception {
-        
-        ZkClient zkClient = new ZkClient(zkServer.getConnectString(), 30000, 30000, ZKStringSerializer$.MODULE$);
-        
-        final ZkUtils zkUtils = new ZkUtils(zkClient, new ZkConnection(zkServer.getConnectString()), false);
-        AdminUtils.createTopic(zkUtils, "test", 1, 1, new Properties(), RackAwareMode.Enforced$.MODULE$);
-        
-        Properties clientProps = new Properties();
-        clientProps.put("bootstrap.servers", "localhost:12345");
-        clientProps.put("group.id", "test");
-        clientProps.put("serializer.class", "kafka.serializer.StringEncoder");
-        clientProps.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-        clientProps.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-        clientProps.put("acks", "all");
-        
-        KafkaConsumer<String, String> consumer = new KafkaConsumer
-            <String, String>(clientProps);
-        consumer.subscribe(Arrays.asList("test"));
-        
-        Properties props = new Properties();
-        props.put("metadata.broker.list", "localhost:12345");
-        props.put("bootstrap.servers", "localhost:12345");
-        props.put("acks", "all");
-        props.put("serializer.class", "kafka.serializer.StringEncoder");
-        //props.put("producer.type", "async");
-        props.put("batch.size", "1");
-        props.put("request.required.acks", "1");
-        props.put("client.id", "producertest");
-        props.put("key.serializer", 
-            "org.apache.kafka.common.serialization.StringSerializer");
-         props.put("value.serializer", 
-            "org.apache.kafka.common.serialization.StringSerializer");
-         
-        
-        ProducerConfig config = new ProducerConfig(props);
-        Producer<String, String> producer = new Producer<String, String>(config);
-        producer.send(new KeyedMessage<String, String>("test","blah2"));
-        producer.close();
-        
-        Thread.sleep(1000);
-        
-        ConsumerRecords<String, String> records = consumer.poll(100);
-        System.out.println("REC: " + records.count());
-        for (ConsumerRecord<String, String> record : records)
-            
-            // print the offset,key and value for the consumer records.
-            System.out.printf("BLAH offset = %d, key = %s, value = %s\n", 
-               record.offset(), record.key(), record.value());
-        
-    }
-    
-    private static KafkaConfig getKafkaConfig(final String zkConnectString) {
-        /*
-        scala.collection.Iterator<Properties> propsI =
-            TestUtils.createBrokerConfigs(1, true).iterator();
-        assert propsI.hasNext();
-        Properties props = propsI.next();
-        assert props.containsKey("zookeeper.connect");
-        // Props
-        //KEY: controlled.shutdown.enable true
-        //KEY: port 41085
-        //KEY: broker.id 0
-        //KEY: host.name localhost
-        //KEY: zookeeper.connect 127.0.0.1:43422
-        //KEY: replica.socket.timeout.ms 1500
-        //KEY: log.dir /tmp/kafka-115764
-        */
         
         Properties props = new Properties();
         props.put("broker.id", 1);
         props.put("host.name", "localhost");
         props.put("port", "12345");
         props.put("log.dir", "/tmp/kafka");
-        props.put("zookeeper.connect", zkConnectString);
+        props.put("zookeeper.connect", zkServer.getConnectString());
         props.put("replica.socket.timeout.ms", "1500");
         props.put("controlled.shutdown.enable", Boolean.TRUE.toString());
 
-        // props.put("zookeeper.connect", zkConnectString);
-        return new KafkaConfig(props);
-        /*
-        Properties props = new Properties();
-        props.put("port", "9092");
-        props.put("broker.id", "1");
-        props.put("log.dir", Files.createTempDir());
-        props.put("zookeeper.connect",zkConnectString);
-        KafkaConfig kafkaConfig = new KafkaConfig(props);
-        return kafkaConfig;
-        */
+        KafkaConfig config = new KafkaConfig(props);
+        kafkaServer = new KafkaServerStartable(config);
+        kafkaServer.startup();
     }
+    
+    @org.junit.AfterClass
+    public static void cleanup() throws Exception {
+        if (kafkaServer != null) {
+            kafkaServer.shutdown();
+        }
+        if (zkServer != null) {
+            zkServer.stop();
+        }
+    }
+    
+    @org.junit.Test
+    public void testKafka() throws Exception {
+        
+        // Create a "test" topic
+        ZkClient zkClient = new ZkClient(zkServer.getConnectString(), 30000, 30000, ZKStringSerializer$.MODULE$);
+        
+        final ZkUtils zkUtils = new ZkUtils(zkClient, new ZkConnection(zkServer.getConnectString()), false);
+        AdminUtils.createTopic(zkUtils, "test", 1, 1, new Properties(), RackAwareMode.Enforced$.MODULE$);
+        
+        // Create the Producer
+        Properties producerProps = new Properties();
+        producerProps.put("bootstrap.servers", "localhost:12345");
+        producerProps.put("acks", "all");
+        producerProps.put("retries", 0);
+        producerProps.put("batch.size", 16384);
+        producerProps.put("linger.ms", 1);
+        producerProps.put("buffer.memory", 33554432);
+        producerProps.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        producerProps.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        
+        Producer<String, String> producer = new KafkaProducer<>(producerProps);
+
+        // Create the Consumer
+        Properties consumerProps = new Properties();
+        consumerProps.put("bootstrap.servers", "localhost:12345");
+        consumerProps.put("group.id", "test");
+        consumerProps.put("enable.auto.commit", "true");
+        consumerProps.put("auto.commit.interval.ms", "1000");
+        consumerProps.put("session.timeout.ms", "30000");
+        consumerProps.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        consumerProps.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(consumerProps);
+        consumer.subscribe(Arrays.asList("test"));
+        
+        // Send a message
+        producer.send(new ProducerRecord<String, String>("test", "somekey", "somevalue"));
+        producer.flush();
+        
+        // Poll until we consume it
+        ConsumerRecord<String, String> record = null;
+        for (int i = 0; i < 1000; i++) {
+            ConsumerRecords<String, String> records = consumer.poll(100);
+            if (records.count() > 0) {
+                record = records.iterator().next();
+                break;
+            }
+        }
+        
+        Assert.assertNotNull(record);
+        Assert.assertEquals("somevalue", record.value());
+        
+        producer.close();
+        consumer.close();
+    }
+    
 }
