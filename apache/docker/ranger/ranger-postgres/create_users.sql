1d57e9dc91b7f0b33a38533c7869f0d907608e30
Colm O hEigeartaigh <coheigea@apache.org>
2018-07-11 12:28:15 +0100

    Revert "Also creating ranger-kms database"
    This reverts commit b71c9a395f0030b749d2dc75cb179b2be1856833.

--- apache/docker/ranger/ranger-postgres/create_users.sql
+++ apache/docker/ranger/ranger-postgres/create_users.sql
@@ -1,5 +1,4 @@
 CREATE USER ranger WITH PASSWORD 'security';
 CREATE USER admin WITH PASSWORD 'security';
 CREATE DATABASE ranger;
-CREATE DATABASE rangerkms;
 GRANT ALL PRIVILEGES ON DATABASE ranger to admin;
