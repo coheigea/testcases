0a8f8cb3564b210b99b487c9c35b72eb0346a564
Colm O hEigeartaigh <coheigea@apache.org>
2019-01-29 19:05:42 +0000

    Adding STS Jmeter tests

--- apache/cxf/cxf-benchmarks/cxf-jmeter/src/test/java/org/apache/coheigea/cxf/jmeter/sts/CommonCallbackHandler.java
+++ apache/cxf/cxf-benchmarks/cxf-jmeter/src/test/java/org/apache/coheigea/cxf/jmeter/sts/CommonCallbackHandler.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.coheigea.cxf.jmeter.common;
+package org.apache.coheigea.cxf.jmeter.sts;
 
 import java.io.IOException;
 
