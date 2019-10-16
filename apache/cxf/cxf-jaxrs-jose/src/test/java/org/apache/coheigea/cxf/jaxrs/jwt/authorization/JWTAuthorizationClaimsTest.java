653bf4e4eda8869e1e9409c04e41950cf7cf3292
Colm O hEigeartaigh <coheigea@apache.org>
2019-10-16 17:38:59 +0100

    Fixing keys

--- apache/cxf/cxf-jaxrs-jose/src/test/java/org/apache/coheigea/cxf/jaxrs/jwt/authorization/JWTAuthorizationClaimsTest.java
+++ apache/cxf/cxf-jaxrs-jose/src/test/java/org/apache/coheigea/cxf/jaxrs/jwt/authorization/JWTAuthorizationClaimsTest.java
@@ -174,10 +174,10 @@ public class JWTAuthorizationClaimsTest extends AbstractBusClientServerTestBase 
 
         Map<String, Object> properties = new HashMap<String, Object>();
         properties.put("rs.security.keystore.type", "jks");
-        properties.put("rs.security.keystore.password", "ispass");
-        properties.put("rs.security.keystore.alias", "imposter");
-        properties.put("rs.security.keystore.file", "imposter.jks");
-        properties.put("rs.security.key.password", "ikpass");
+        properties.put("rs.security.keystore.password", "cspass");
+        properties.put("rs.security.keystore.alias", "myclientkey");
+        properties.put("rs.security.keystore.file", "clientstore.jks");
+        properties.put("rs.security.key.password", "ckpass");
         properties.put("rs.security.signature.algorithm", "RS256");
         properties.put(JwtConstants.JWT_TOKEN, token);
         
@@ -217,10 +217,10 @@ public class JWTAuthorizationClaimsTest extends AbstractBusClientServerTestBase 
 
         Map<String, Object> properties = new HashMap<String, Object>();
         properties.put("rs.security.keystore.type", "jks");
-        properties.put("rs.security.keystore.password", "ispass");
-        properties.put("rs.security.keystore.alias", "imposter");
-        properties.put("rs.security.keystore.file", "imposter.jks");
-        properties.put("rs.security.key.password", "ikpass");
+        properties.put("rs.security.keystore.password", "cspass");
+        properties.put("rs.security.keystore.alias", "myclientkey");
+        properties.put("rs.security.keystore.file", "clientstore.jks");
+        properties.put("rs.security.key.password", "ckpass");
         properties.put("rs.security.signature.algorithm", "RS256");
         properties.put(JwtConstants.JWT_TOKEN, token);
         
