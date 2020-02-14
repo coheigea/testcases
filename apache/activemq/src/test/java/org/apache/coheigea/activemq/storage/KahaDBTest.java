a938370db06c73a9c281f96b4e639f77c5edce3a
Colm O hEigeartaigh <coheigea@apache.org>
2020-02-14 12:19:07 +0000

    Adding some storage tests

---
+++ apache/activemq/src/test/java/org/apache/coheigea/activemq/storage/KahaDBTest.java
@@ -0,0 +1,95 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.coheigea.activemq.storage;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.net.ServerSocket;
+
+import javax.jms.Connection;
+import javax.jms.DeliveryMode;
+import javax.jms.Destination;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.broker.BrokerService;
+import org.apache.activemq.store.kahadb.KahaDBStore;
+
+/**
+ * Basic test for message producing + consumer over TCP using KahaDB to store the messages
+ */
+public class KahaDBTest {
+    
+    private static BrokerService broker;
+    private static String brokerAddress;
+    
+    @org.junit.BeforeClass
+    public static void startBroker() throws Exception {
+        
+        broker = new BrokerService();
+        
+        KahaDBStore kaha = new KahaDBStore();
+        kaha.setDirectory(new File("target/activemq-kahadb"));
+        broker.setPersistenceAdapter(kaha);
+        
+        ServerSocket serverSocket = new ServerSocket(0);
+        int brokerPort = serverSocket.getLocalPort();
+        serverSocket.close();
+        
+        brokerAddress = "tcp://localhost:" + brokerPort;
+        broker.addConnector(brokerAddress);
+        broker.start();
+    }
+    
+    @org.junit.AfterClass
+    public static void stopBroker() throws Exception {
+        if (broker != null) {
+            broker.stop();
+        }
+    }
+    
+    @org.junit.Test
+    public void testBasicTextMessage() throws Exception {
+        ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory(brokerAddress);
+        Connection connection = factory.createConnection();
+        connection.start();
+        
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        Destination queue = session.createQueue("testqueue");
+        MessageProducer producer = session.createProducer(queue);
+        producer.setDeliveryMode(DeliveryMode.PERSISTENT);
+        
+        TextMessage message = session.createTextMessage("Some txt");
+        message.setStringProperty("some header", "some value");
+        
+        producer.send(message);
+        
+        MessageConsumer consumer = session.createConsumer(queue);
+        TextMessage receivedMessage = (TextMessage)consumer.receive(1000L);
+        assertEquals("Some txt", receivedMessage.getText());
+        assertEquals("some value", receivedMessage.getStringProperty("some header"));
+        
+        connection.close();
+    }
+    
+}
