df897ee844ceaf84a486eb38aa9f9a9f5ec6b5d7
Colm O hEigeartaigh <coheigea@apache.org>
2020-03-05 10:04:05 +0000

    Adding MapMessage test

---
+++ apache/activemq/src/test/java/org/apache/coheigea/activemq/misc/MapMessageTest.java
@@ -0,0 +1,89 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.coheigea.activemq.misc;
+
+import static org.junit.Assert.assertArrayEquals;
+
+import java.net.ServerSocket;
+
+import javax.jms.Connection;
+import javax.jms.Destination;
+import javax.jms.MapMessage;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.broker.BrokerService;
+import org.apache.activemq.store.memory.MemoryPersistenceAdapter;
+
+/**
+ * Basic test for message producing + consumer over TCP using a MapMessage
+ */
+public class MapMessageTest {
+    
+    private static BrokerService broker;
+    private static String brokerAddress;
+    
+    @org.junit.BeforeClass
+    public static void startBroker() throws Exception {
+        
+        broker = new BrokerService();
+        broker.setPersistenceAdapter(new MemoryPersistenceAdapter());
+        broker.setDataDirectory("target/activemq-data");
+        
+        ServerSocket serverSocket = new ServerSocket(0);
+        int brokerPort = serverSocket.getLocalPort();
+        serverSocket.close();
+        
+        brokerAddress = "tcp://localhost:" + brokerPort;
+        broker.addConnector(brokerAddress);
+        broker.start();
+    }
+    
+    @org.junit.AfterClass
+    public static void stopBroker() throws Exception {
+        if (broker != null) {
+            broker.stop();
+        }
+    }
+    
+    @org.junit.Test
+    public void testBasicTextMessage() throws Exception {
+        ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory(brokerAddress);
+        Connection connection = factory.createConnection();
+        connection.start();
+        
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        Destination queue = session.createQueue("testqueue");
+        MessageProducer producer = session.createProducer(queue);
+        
+        MapMessage message = session.createMapMessage();
+        message.setBytes("key", "some value".getBytes());
+        
+        producer.send(message);
+        
+        MessageConsumer consumer = session.createConsumer(queue);
+        MapMessage receivedMessage = (MapMessage)consumer.receive(1000L);
+        assertArrayEquals("some value".getBytes(), receivedMessage.getBytes("key"));
+        
+        connection.close();
+    }
+    
+}
